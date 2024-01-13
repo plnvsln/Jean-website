384821a2832b51e27b258f01a513104dfe5b245e
plnvsln <mr.mightypo@gmail.com>
2024-01-13 10:13:20 +0000

    added elements to gallery-sale page

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const scrollers = document.querySelectorAll(".scroller");
+// const scrollers = document.querySelectorAll(".scroller");
 
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-  addAnimation();
-}
-// checking whether user has opted for reduced motion
-function addAnimation() {
-  scrollers.forEach((scroller) => {
-    scroller.setAttribute("data-animated", true);
-  });
-}
+// if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+//   addAnimation();
+// }
+// // checking whether user has opted for reduced motion
+// function addAnimation() {
+//   scrollers.forEach((scroller) => {
+//     scroller.setAttribute("data-animated", true);
+//   });
+// }
